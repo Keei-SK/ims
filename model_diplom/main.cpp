a4d4fb8c5b63ae0987afb9e963c0ccb4f0a2077c
MarekPetr <petr.marek18@gmail.com>
2019-12-06 15:00:17 +0100

    Math model

--- model_diplom/main.cpp
+++ model_diplom/main.cpp
@@ -9,41 +9,77 @@ using namespace std;
 class Model {
 public:
     Model(Arguments args);
-    Arguments param;
-    int newborn_females = 0; //n0(t+1)    
-    double beta0 = 0; //bi(t) - female of age i has in month t, bi daughters
-    double beta1 = 0;
-    int all_females = 200;
-    int females_of_age[MAX_AGE+1];
-    int num_daughters_in_month = 0;
-    double p = 0.99;
-
-    int get_num_daughters_in_month();
-    int get_newborn_females();
-    void update_females_sum();
     void simulate();
-    int get_all_females();
-    double getBeta0();
+
+private:
+    Arguments param;
+    double newborn_females = 0; //n0(t+1)    
+    double beta0 = 0;
+    double beta1 = 0;
+    double gamma0 = 0;
+    double gamma1 = 0;
+    double all_females = 0;         //N(t)
+    double females_of_age[MAX_AGE+1]; //ni(t)
+    double coef_surv[MAX_AGE+1]  = {0.95, 0.93, 0.90, 0.88, 0.86, 0.85, 0.82, 0.78, 0.75, 0.60, 0.50, 0.40, 0.30, 0.27, 0.25};
+    double num_daughters_in_month = 0;
+    double p = 0.99;
+    double max_min = 0;
+    int order = 0;
+
+    double get_num_daughters_in_month();
+    double get_newborn_females();
+    
+    double get_all_females();
+    double get_females_of_age();
+    double get_newborn_females_cnt_per_age(int age);
+    //double get_coef_surv();
+    double get_gamma_function_result(int age);
+    double new_num_females_of_age();
 };
 
 Model::Model(Arguments args) {
     param = args;
-    cout << param.max_daughters_month << endl;
-    cout << "init" << endl;
     for (int i = 0; i <= MAX_AGE; ++i)
     {
-        females_of_age[i] = 2;
+        females_of_age[i] = i;
     }
     beta0 = log(p/(1-p));
-    beta1 = -(log((2-p)/(1-p))/1000);
+    if (param.dens_b_half != 0) {
+        beta1 = -(log((2-p)/(1-p)))/param.dens_b_half;
+    } else {
+        beta1 = 0;
+    }
+
+    max_min = param.max_surv - param.min_surv;
+    gamma0 = log(p/(1-p));
+    if (param.mother_age_repro != 0) {
+        gamma1 = -(log(2-p))/param.mother_age_repro;
+    } else {
+        gamma1 = 0;
+    }
 }
 
-void Model::simulate(){
-    cout << get_newborn_females() << endl;
+void Model::simulate() {
+    double dens_repro_delayed = 0;
+    for (int i = 0; i < 100; ++i)
+    {
+        int month = i % 12;
+
+        cout << get_all_females() << endl;
+        new_num_females_of_age();
+        if (month >= 0 && month < 8) {
+            females_of_age[0] = get_newborn_females();
+        }        
+    }    
+
+    //get_coef_surv();
+    /*cout << "num_daughters_in_month " << num_daughters_in_month << endl;
+    cout << "newborn_females " << newborn_females << endl;*/
 }
 
-int Model::get_all_females() {
-    int sum = 0;
+// N(t)
+double Model::get_all_females() {
+    double sum = 0;
     for (int i = 0; i <= MAX_AGE; i++)
     {
         sum += females_of_age[i];
@@ -53,17 +89,20 @@ int Model::get_all_females() {
 }
 
 // bi(t)
-int Model::get_num_daughters_in_month() {
-    double power_e = pow(M_E,(beta0+beta1*get_all_females()));
-    num_daughters_in_month = param.max_daughters_month*(power_e/(1+power_e));
+double Model::get_num_daughters_in_month() {
+    get_all_females();
+    double power_e = pow(M_E,(beta0+beta1*all_females));
+    /*cout << "beta1:" << beta1 << endl;
+    cout << "power_e " << power_e << endl;
+    cout << "pow: " << param.max_daughters_month*(double)(power_e/(1+power_e)) << endl;*/
+    num_daughters_in_month = param.max_daughters_month*(double)(power_e/(1+power_e));
     return num_daughters_in_month;
 }
 
 // n0(t+1)
-int Model::get_newborn_females() {
-    cout << "calc_newborn_females" << endl;
+double Model::get_newborn_females() {
     get_num_daughters_in_month();
-    int sum = 0;
+    double sum = 0;
     for (int i = 0; i <= MAX_AGE-4; i++)
     {
         sum += num_daughters_in_month*females_of_age[i];
@@ -71,6 +110,52 @@ int Model::get_newborn_females() {
     newborn_females = sum;
     return newborn_females;
 }
+// wi(t)
+double Model::get_newborn_females_cnt_per_age(int age) {
+    if (newborn_females != 0) {
+        return (num_daughters_in_month*females_of_age[age])/females_of_age[0];
+    }
+    return 0;
+}
+
+/*// complicated formula at the end of s0(t+1)
+double Model::get_gamma_function_result(int age) {
+    cout << "females of age: " << females_of_age[age] << endl;
+    double numerator = pow(M_E,gamma0 +(gamma1*females_of_age[age]));
+    double denominator = 1 + pow(M_E,gamma0 +(gamma1*females_of_age[age]));
+    cout << "numerator:" << numerator << endl;
+    cout << "denom:" << denominator << endl;
+    if (denominator != 0) {
+        //cout << "zlomek: " << numerator/denominator << endl;
+        return numerator/denominator;
+    }  
+    else 
+        return 0;
+}*/
+
+/*// Sk(t)
+double Model::get_coef_surv() {
+    // ai(t) - number of mothers of age i
+    double sum = 0;
+    for (int i = 0; i <= MAX_AGE; i++)
+    {   
+        cout << "newbown: " << get_newborn_females_cnt_per_age(i) << endl;
+        sum += get_newborn_females_cnt_per_age(i)*(param.min_surv + max_min*get_gamma_function_result(i));
+    }
+    cout << "sum s0: " << sum << endl;
+    return sum;
+}*/
+
+// nk(t+1) - number of females k months old
+double Model::new_num_females_of_age() {
+    
+   for (int i = MAX_AGE-1; i >= 0; i--)
+    {
+        females_of_age[i+1] = coef_surv[i]*females_of_age[i];
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char* argv[]) {
     Arguments args;
