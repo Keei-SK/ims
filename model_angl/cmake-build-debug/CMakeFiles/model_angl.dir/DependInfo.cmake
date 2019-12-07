4a9332564205fc7c5b27b7e2ae23c49a1fdbff5c
Jozef Vanický <jozef.vanicky@gmail.com>
2019-12-07 20:12:25 +0100

    Makefile edit, add Cmake for Clion

---
+++ model_angl/cmake-build-debug/CMakeFiles/model_angl.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jozefvanicky/Documents/Projects/IMS/model_angl/cell.cpp" "/Users/jozefvanicky/Documents/Projects/IMS/model_angl/cmake-build-debug/CMakeFiles/model_angl.dir/cell.cpp.o"
+  "/Users/jozefvanicky/Documents/Projects/IMS/model_angl/grid.cpp" "/Users/jozefvanicky/Documents/Projects/IMS/model_angl/cmake-build-debug/CMakeFiles/model_angl.dir/grid.cpp.o"
+  "/Users/jozefvanicky/Documents/Projects/IMS/model_angl/main.cpp" "/Users/jozefvanicky/Documents/Projects/IMS/model_angl/cmake-build-debug/CMakeFiles/model_angl.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
